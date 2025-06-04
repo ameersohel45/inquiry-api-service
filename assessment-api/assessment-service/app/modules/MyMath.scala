299989b4e74c834fc975c06bfd401762cf1c28e2
ameersohel45-desktop <sohel.cse.rymec@gmail.com>
2025-06-04 15:11:02 +0530

    new code added

---
+++ assessment-api/assessment-service/app/modules/MyMath.scala
@@ -0,0 +1,14 @@
+package modules
+// src/main/scala/com/example/MyMath.scala
+
+
+object MyMath {
+  def add(a: Int, b: Int): Int = {
+    a + b
+  }
+
+  // --- ADD THIS NEW FUNCTION ---
+  def multiply(a: Int, b: Int): Int = {
+    a * b
+  }
+}
